ae1b39f07a141c4b6361cd424c9608ea40d4244f
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-11-01 15:59:24 +0100

    body-parser installed, implemented search with text

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const apiUrl = "https://api.potterdb.com/v1";
 
@@ -23,10 +25,32 @@ app.get("/books/page/:page", async (req, res) => {
     }
 })
 
+app.get("/books/search", async (req, res) => {
+    try {
+        const wordToSearch = req.query.wordToSearch;
+        const result = await axios.get(`${apiUrl}/books?filter[title_cont]=${wordToSearch}`);
+        res.render("index.ejs", {result: result.data});
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
+
 app.get("/characters/page/:page", async (req, res) => {
     try {
         const page = req.params.page;
         const result = await axios.get(`${apiUrl}/characters?page[number]=${page}&page[size]=25`);
+        res.render("index.ejs", {result: result.data});
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
+
+app.get("/characters/search", async (req, res) => {
+    try {
+        const wordToSearch = req.query.wordToSearch;
+        const result = await axios.get(`${apiUrl}/characters?filter[name_cont]=${wordToSearch}`);
         res.render("index.ejs", {result: result.data});
     } catch (error) {
         console.log(error.response.data);
@@ -45,6 +69,17 @@ app.get("/movies/page/:page", async (req, res) => {
     }
 })
 
+app.get("/movies/search", async (req, res) => {
+    try {
+        const wordToSearch = req.query.wordToSearch;
+        const result = await axios.get(`${apiUrl}/movies?filter[title_cont]=${wordToSearch}`);
+        res.render("index.ejs", {result: result.data});
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
+
 app.get("/potions/page/:page", async (req, res) => {
     try {
         const page = req.params.page;
@@ -56,10 +91,32 @@ app.get("/potions/page/:page", async (req, res) => {
     }
 })
 
+app.get("/potions/search", async (req, res) => {
+    try {
+        const wordToSearch = req.query.wordToSearch;
+        const result = await axios.get(`${apiUrl}/potions?filter[name_cont]=${wordToSearch}`);
+        res.render("index.ejs", {result: result.data});
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
+
 app.get("/spells/page/:page", async (req, res) => {
     try {
         const page = req.params.page;
         const result = await axios.get(`${apiUrl}/spells?page[number]=${page}&page[size]=25`);
+        res.render("index.ejs", {result: result.data});
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
+
+app.get("/spells/search", async (req, res) => {
+    try {
+        const wordToSearch = req.query.wordToSearch;
+        const result = await axios.get(`${apiUrl}/spells?filter[name_cont]=${wordToSearch}`);
         res.render("index.ejs", {result: result.data});
     } catch (error) {
         console.log(error.response.data);
